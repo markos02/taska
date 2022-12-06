8ef9f3e3e9f2668f25726e2bde1cc21a4e2c2eda
mk <kosakowski.m@gmail.com>
2022-12-06 21:19:27 +0100

    19.2 Zadanie: wyszukiwanie pojedynczego zadania

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,23 +1,32 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
 
-    @GetMapping(value = "getTasks")
+    private final DbService dbService;
+    private final TaskMapper taskMapper;
+
+    @GetMapping()
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> task = dbService.getAllTasks();
+        return taskMapper.mapToTaskDtoList(task);
     }
 
-    @GetMapping(value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        Task task = dbService.getTask(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping

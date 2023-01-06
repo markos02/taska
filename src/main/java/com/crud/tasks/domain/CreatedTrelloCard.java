f043c3ca0937aa291c61a09920459253ba3167ce
mk <kosakowski.m@gmail.com>
2023-01-06 18:23:29 +0100

    22.4. Przygotowania endpointa do eksportowania zadań

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -2,9 +2,12 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
 
@@ -19,4 +22,10 @@ public class CreatedTrelloCard {
 
     @JsonProperty("badges")
     private TrelloBadgesDto badges;
+
+    public CreatedTrelloCard(String id, String name, String shortUrl) {
+        this.id = id;
+        this.name = name;
+        this.shortUrl = shortUrl;
+    }
 }

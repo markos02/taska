bba51dfc9f702320d146604e60bf09798762e06d
mk <kosakowski.m@gmail.com>
2023-03-18 13:39:43 +0100

    32.3. Zadanie: Wykonaj szablon maila

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -18,6 +18,7 @@ public class EmailScheduler {
     private final AdminConfig adminConfig;
 
     @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String message = "Currently in database you got: " + size;
